8739699443725e1a9ab9573f9142dd02f66c0d32
Steven Mahana <stevenmahana@gmail.com>
2016-08-28 19:23:14 -0700

    added custom listen method

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express')
     , bodyParser = require('body-parser')
     , app = express()
     , sync = require('synchronize')
-    , cors = require('cors');
+    , cors = require('cors')
+    , port = process.env.PORT || 9145;
 
 // Use fibers in all routes so we can use sync.await() to make async code easier to work with.
 app.use(function(req, res, next) {
@@ -15,7 +16,17 @@ var corsOptions = {
   credentials: true
 };
 
+app.get('/', function (req, res) {res.send('iTunes Slash Command for Mixmax');});
 app.get('/typeahead', cors(corsOptions), require('./api/typeahead'));
 app.get('/resolver', cors(corsOptions), require('./api/resolver'));
 
-app.listen(process.env.PORT || 9145);
+app.listen(port, function(err) {
+  if (err) {
+    // Crash the app if the port is in use, etc.
+    throw err;
+  }
+  console.log('Listening on port %d', port);
+  console.log('http://localhost:' + port + '/');
+  console.log('http://localhost:' + port + '/typeahead?text=jack');
+  console.log('http://localhost:' + port + '/resolver?text=297957761');
+});
